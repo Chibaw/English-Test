c7ed5510e5f3d322b5d94d6abb00a94e4bbe8bed
Julien Bordellier <julienbordellier@gmail.com>
2012-07-22 16:40:46 +0200

    Beerware License headers, gitignore and beginning of README file

--- AnglaisTest1/Users.m
+++ AnglaisTest1/Users.m
@@ -1,10 +1,11 @@
-//
-//  Users.m
-//  AnglaisTest1
-//
-//  Created by Julien Bordellier on 17/07/12.
-//  Copyright (c) 2012 Epitech. All rights reserved.
-//
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bordel@epitech.eu> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Julien Bordellier
+ * ----------------------------------------------------------------------------
+ */
 
 #import "Users.h"
 
