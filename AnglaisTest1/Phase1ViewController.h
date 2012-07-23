50fb3ef37c6e6b66fa4ca56e018ebc66630c60f7
Julien Bordellier <julienbordellier@gmail.com>
2012-07-23 13:54:10 +0200

    Ajout du ViewControlleur pour le toeic et suppression du data core

---
+++ AnglaisTest1/Phase1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Phase1ViewController.h
+//  AnglaisTest1
+//
+//  Created by Julien Bordellier on 23/07/12.
+//  Copyright (c) 2012 Epitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Phase1ViewController : UIViewController
+
+@end
