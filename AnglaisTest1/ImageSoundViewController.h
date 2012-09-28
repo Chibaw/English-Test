1c7cc0db709a30ef259e3e51d4a700c4ec044673
Julien Bordellier <julienbordellier@gmail.com>
2012-09-29 00:56:17 +0200

    Licences fix

--- AnglaisTest1/ImageSoundViewController.h
+++ AnglaisTest1/ImageSoundViewController.h
@@ -1,10 +1,11 @@
-//
-//  ImageSoundViewController.h
-//  AnglaisTest1
-//
-//  Created by Julien Bordellier on 27/09/12.
-//  Copyright (c) 2012 Epitech. All rights reserved.
-//
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bordel@epitech.eu> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Julien Bordellier
+ * ----------------------------------------------------------------------------
+ */
 
 #import <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
